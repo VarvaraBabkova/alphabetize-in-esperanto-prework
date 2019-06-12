1c61f9544b06d9a7c8b1c5f94acdf870566f84e3
Varvara Babkova <varvara.munday@gmail.com>
2019-06-12 22:23:11 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,16 +1,23 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-def test (x)
+def letter_index (x)
   return ESPERANTO_ALPHABET.index(x)
+end
+
+def test_word (x, y)
+  return 0 if x == y
+
+  min_size = (x.size<y.size) ? x.size : y.size
+  for i in 0..(min_size - 1) do
+    return 1 if letter_index(x[i]) >  letter_index(y[i])
+    return -1 if letter_index(x[i]) <  letter_index(y[i])
+  end
 end
 
 def alphabetize(arr)
   new_arr = arr
   new_arr.sort! do |x, y|
-    min_size = (x.size<y.size) ? x.size : y.size
-    for i in 0..(min_size - 1) do
-      test(x[i]) <=> test(y[i])
-    end
+    test_word(x, y)
   end
   return new_arr
 end
