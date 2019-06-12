d17b111c25739097dd040511757c38b1e87f327f
Varvara Babkova <varvara.munday@gmail.com>
2019-06-12 22:15:04 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -8,7 +8,7 @@ def alphabetize(arr)
   new_arr = arr
   new_arr.sort! do |x, y|
     min_size = (x.size<y.size) ? x.size : y.size
-    for i in 0..min_size - 1 do
+    for i in 0..(min_size - 1) do
       test(x[i]) <=> test(y[i])
     end
   end
